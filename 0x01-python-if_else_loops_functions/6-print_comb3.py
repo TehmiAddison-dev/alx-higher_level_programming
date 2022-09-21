94b1014b9b8e563fc20954f4a1d93061a0fe19bc
ASHAOLU Temitope <temitopeashaolu@yahoo.co.uk>
2022-09-21 09:27:48 +0100

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i, 10):
-        if i < j:
-            print("{:d}{:d}".format(i, j),
-                  end="\n" if i is 8 and j is 9 else ", ")
+for num in range(9):
+    for var in range(num + 1, 10):
+        print("{:d}{:d}".format(num, var), end='\n' if num == 8 else ', ')
+        
