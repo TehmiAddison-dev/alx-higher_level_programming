88f2c5a9f9498a4074d90298fb7bd82a9bae23a4
TehmiAddison-dev <temitopeashaolu@yahoo.co.uk>
2022-09-28 05:33:10 -0700

    7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -6,4 +6,4 @@ def update_dictionary(a_dictionary, key, value):
     updates or add key/value into a dictionary and returns a new copy
     """
     a_dictionary.update({key: value})
-    return (a_dictionary.copy()
+    return (a_dictionary.copy())
