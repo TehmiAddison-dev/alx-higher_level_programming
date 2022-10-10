a62b95585345b12a4b6ffe048587aff1a2385ee6
ASHAOLU Temitope <temitopeashaolu@yahoo.co.uk>
2022-10-10 14:46:48 +0100

    tests.py

--- 0x05-python-exceptions/tests.py
+++ 0x05-python-exceptions/tests.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/python3 -u
+
 import ctypes
 
 lib = ctypes.CDLL('./libPython.so')
 lib.print_python_list.argtypes = [ctypes.py_object]
 lib.print_python_bytes.argtypes = [ctypes.py_object]
 lib.print_python_float.argtypes = [ctypes.py_object]
-
 s = b"Hello"
 lib.print_python_bytes(s);
 b = b'\xff\xf8\x00\x00\x00\x00\x00\x00';
@@ -13,10 +13,10 @@ lib.print_python_bytes(b);
 b = b'What does the \'b\' character do in front of a string literal?';
 lib.print_python_bytes(b);
 l = [b'Hello', b'World']
-#lib.print_python_list(l)
+lib.print_python_list(l)
 del l[1]
 lib.print_python_list(l)
-l = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], b"Holberton", "Betty"]
+l = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], b"School", "Betty"]
 lib.print_python_list(l)
 l = []
 lib.print_python_list(l)
@@ -29,7 +29,7 @@ l.append(4)
 lib.print_python_list(l)
 l.pop()
 lib.print_python_list(l)
-l = ["Holberton"]
+l = ["School"]
 lib.print_python_list(l)
 lib.print_python_bytes(l);
 f = 3.14
