5d4084d5db1f45e1fde30acfce798bd695213dd3
ASHAOLU Temitope <temitopeashaolu@yahoo.co.uk>
2022-10-17 13:39:42 +0100

    6-max_integer.py

--- 0x07-python-test_driven_development/6-max_integer.py
+++ 0x07-python-test_driven_development/6-max_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Module to find the max integer in a list
+"""
+
 
 def max_integer(list=[]):
     """Function to find and return the max integer in a list of integers
@@ -13,4 +16,3 @@ def max_integer(list=[]):
             result = list[i]
         i += 1
     return result
-  
