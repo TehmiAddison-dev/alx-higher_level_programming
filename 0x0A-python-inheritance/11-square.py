c14effb56b66a4da8154d59080c0912a2ccccfa8
ASHAOLU Temitope <temitopeashaolu@yahoo.co.uk>
2022-10-24 13:37:16 +0100

    11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
-def is_same_class(obj, a_class):
-    return (0)
-print(is_same_class(obj, a_class))
+"""Defines a Rectangle subclass Square."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
