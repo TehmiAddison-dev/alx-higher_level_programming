561cf8f23dcf5b2faadcf802120b4b3c8e77eb14
ASHAOLU Temitope <temitopeashaolu@yahoo.co.uk>
2022-10-25 13:12:31 +0100

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
+"""Defines a text file-reading function."""
+
+
 def read_file(filename=""):
+    """Print the contents of a UTF8 text file to stdout."""
     with open(filename, encoding="utf-8") as f:
-        lineNum = 1
-        while(True):
-            line = f.readline()
-            if not line:
-                break
-            print(line, end="")
-            lineNum += 1
+        print(f.read(), end="")
