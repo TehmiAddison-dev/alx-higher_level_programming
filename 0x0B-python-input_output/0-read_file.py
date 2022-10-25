7cc74943fd388a6d123ccb1950c7d73013fa24e5
ASHAOLU Temitope <temitopeashaolu@yahoo.co.uk>
2022-10-25 12:53:17 +0100

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+def read_file(filename=""):
+    """
+    Reads the file
+    Arguments:
+        filename (str): The name of the file to open
+    """
+    with open(filename, encoding='utf-8') as file:
+        print(file.read(), end='')
