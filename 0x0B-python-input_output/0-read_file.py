809093684b62e9e8b52f085825d0833c30f03bd1
ASHAOLU Temitope <temitopeashaolu@yahoo.co.uk>
2022-10-25 12:55:23 +0100

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-
 def read_file(filename=""):
-    """
-    Reads the file
-    Arguments:
-        filename (str): The name of the file to open
-    """
-    with open(filename, encoding='utf-8') as file:
-        print(file.read(), end='')
+    with open(filename, encoding="utf-8") as f:
+        lineNum = 1
+        while(True):
+            line = f.readline()
+            if not line:
+                break
+            print(line, end="")
+            lineNum += 1
